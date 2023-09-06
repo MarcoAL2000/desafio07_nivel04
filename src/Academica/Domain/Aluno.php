146d48fbd08cc8d0b11ced273a74e3cd596440c5
MarcoAL2000 <106031187+MarcoAL2000@users.noreply.github.com>
2023-09-05 21:40:26 -0300

    implementando modulos

---
+++ src/Academica/Domain/Aluno.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Desafio\Academico\Domain;
+
+class Aluno
+{
+    private $nome;
+    private $idade;
+    private $cpf;
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): void
+    {
+        $this->idade = $idade;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): void
+    {
+        $this->cpf = $cpf;
+    }
+
+    private 
+}
